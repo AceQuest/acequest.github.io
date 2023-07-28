7497304911a968d6f39f9403af333432a9b0d03e
dark sorceror <haoyan160@gmail.com>
2023-07-28 11:43:40 -0600

    v1.5.5

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 $(function () {
-    var scroll_pos;
-    var prev_scroll_pos;
+    const version = "v.1.5.5";
+    const public_address = ["https://acequest.github.io/", "https://acequest.github.io/home.html"];
+    const private_address = "http://127.0.0.1:5500/home.html";
 
-    if (window.location.href == "https://acequest.github.io/" || window.location.href == "https://acequest.github.io/index.html" || window.location.href == "http://127.0.0.1:5500/index.html") {
-        alert(window.location.href);
+    var scroll_pos;
+
+    $(".version").replaceWith(version);
+
+    if (public_address.includes(window.location.href) || private_address.includes(window.location.href)) {
         $(document).on("scroll", function() {
             scroll_pos = $(this).scrollTop();
             if (scroll_pos > 50) {
-                $("nav").addClass('scrolled').css("background-color", "#ffffff");
+                $("nav").css("box-shadow", "0px 2px 10px rgba(0, 0, 0, 0.15)");
             } else {
-                $("nav").removeClass('scrolled').css("background-color", "rgb(56, 179, 255)");
-            }
-
-            if ((prev_scroll_pos || prev_scroll_pos == 0) && scroll_pos != prev_scroll_pos) {
-                $(".min-nav").removeClass("active");
+                $("nav").css("box-shadow", "0px 0px 0px rgba(0, 0, 0, 0.15)");
             }
         });
     } else {
-        $("nav").css("box-shadow", "0px 2px 10px rgba(0, 0, 0, 0.15)", "background-color", "#ffffff");
+        $("nav").css("box-shadow", "0px 2px 10px rgba(0, 0, 0, 0.15)");
     }
 
     $(".hamburger").on("click", function() {
@@ -30,5 +30,19 @@ $(function () {
 
     $(".window-button").on("click", function() {
         $(".min-nav-area").removeClass("active").css("z-index", "-1");
-    })
+    });
+
+    $(".hidden-toggle").on("click", function() {
+        $(".hidden-toggle").css("display", "none");
+        $(".visible-toggle").css("display", "flex");
+        $(".password input").attr("type", "password").css("font-family", "pass");
+    });
+
+    $(".visible-toggle").on("click", function() {
+        $(".hidden-toggle").css("display", "flex");
+        $(".visible-toggle").css("display", "none");
+        $(".password input").attr("type", "text").css("font-family", "Montserrat");
+    });
+
+    
 })
