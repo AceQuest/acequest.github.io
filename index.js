196f6abcf1bc3faafdb3b706363216e4d16b35e5
dark sorceror <haoyan160@gmail.com>
2023-07-30 01:10:48 -0600

    v1.5.6 minor changes to the home page

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 $(function () {
-    const version = "v.1.5.5";
-    const public_address = ["https://acequest.github.io/", "https://acequest.github.io/home.html"];
-    const private_address = "http://127.0.0.1:5500/home.html";
+    const version = "v.1.5.6";
+    const public_address = ["https://acequest.github.io/", "https://acequest.github.io/index.html"];
+    const private_address = "http://127.0.0.1:5500/index.html";
 
     var scroll_pos;
 
     $(".version").replaceWith(version);
+    /*Updated nav each page*/
 
     if (public_address.includes(window.location.href) || private_address.includes(window.location.href)) {
         $(document).on("scroll", function() {
             scroll_pos = $(this).scrollTop();
             if (scroll_pos > 50) {
-                $("nav").css("box-shadow", "0px 2px 10px rgba(0, 0, 0, 0.15)");
+                $("nav").css("box-shadow", "0px 2px 10px rgba(0, 0, 0, 0.15)").css("background-color", "#ffffff");
             } else {
-                $("nav").css("box-shadow", "0px 0px 0px rgba(0, 0, 0, 0.15)");
+                $("nav").css("box-shadow", "0px 0px 0px rgba(0, 0, 0, 0.15)").css("background-color", "#add8e6");
             }
         });
     } else {
-        $("nav").css("box-shadow", "0px 2px 10px rgba(0, 0, 0, 0.15)");
+        $("nav").css("background-color", "#ffffff").css("box-shadow", "0px 2px 10px rgba(0, 0, 0, 0.15)");
     }
 
     $(".hamburger").on("click", function() {
