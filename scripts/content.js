ad4d15e594b7a1e74b90087352c7c034ebc004df
dark sorceror <acequestcanada@gmail.com>
2023-10-29 11:48:30 -0600

    v.1.6.7 course format change

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,3 @@
-
-
 const quizDesc = {
     "Polynomial-and-Absolute-Values": {
         "Quiz-1": {
@@ -13,7 +11,23 @@ const quizDesc = {
     }
 }
 
+function animatePercentage(start, end, duration) {
+    let startTimestamp = null;
+    const step = (timestamp) => {
+        if (!startTimestamp) startTimestamp = timestamp;
+        const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+        $('.course-percentage').text(Math.floor(progress * (end - start) + start) + '%');
+        $('.course-details-right svg path.purple').css('stroke-dashoffset', Math.floor(251.5 - (progress * 251.5 * (end / 100  - start) + start)));
+        if (progress < 1) {
+            window.requestAnimationFrame(step);
+        }
+    };
+    window.requestAnimationFrame(step);
+}
+
 $(function () {
+    animatePercentage(0, (238/251.5) * 100, 1500);
+  
     $('.nav-collapse').on('click', function() {
         $('.nav-wrapper').toggleClass('active');
     })
